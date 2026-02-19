c8dca98e03d0747e910f0f139a8070ca186aaf63
MyGit011 <mypc20339@gmail.com>
2026-02-19 10:34:43 +0000

    up

---
+++ Python-08/ex02/oracle.py
@@ -0,0 +1,9 @@
+import os, sys
+
+MATRIX_MODE = os.getenv("MATRIX_MODE")
+# DATABASE_URL=https://github.com/MAN3RAF/Python.git
+# API_KEY=abcd123
+# LOG_LEVEL=DEBUG
+# ZION_ENDPOINT=https://localhost:8000
+
+print(MATRIX_MODE)
