b53e26ecd58e5191db8d69c16c82ec743ad7a321
Lahcen Sebar <lsebar@c3r3p5.1337.ma>
2026-02-24 22:55:01 +0000

    up

--- Python-09/ex1/alien_contact.py
+++ Python-09/ex1/alien_contact.py
@@ -18,19 +18,31 @@ class Validator(BaseModel):
 	timestamp: datetime
 	location: str = Field(min_length=3, max_length=100)
 	contact_type: ContactTypes
-	signal_strength: float = Field(ga=0, le=10)
-	duration_minutes: int = Field(ga=1, le=1440) #(max 24 hours)
-	witness_count: int = Field(ga=1, le=100)
+	signal_strength: float = Field(ge=0, le=10)
+	duration_minutes: int = Field(ge=1, le=1440) #(max 24 hours)
+	witness_count: int = Field(ge=1, le=100)
 	message_received: Optional[str] = Field(max_length=500)
 	is_verified: bool = False
 
 	@model_validator(mode='after')
 	def validate(self):
-		pass
+		allowed = {contact_type.value for contact_type in ContactTypes}
+		if self.contact_type not in allowed:
+			print("ERROR!")
+		if not self.contact_id.startswith("AC"):
+			print("ERROR!")
+		if (self.contact_type == "physical") and not self.is_verified:
+			print("ERROR!")
+		if (self.contact_type == "telepathic") and not (self.witness_count >= 3):
+			print("ERROR!")
+		if self.signal_strength > 7 and not self.message_received:
+			print("ERROR!")
 
 def main():
-	print()
+	
+	print("Alien Contact Log Validation")
+	print("======================================")
 
 
 if __name__ == "__main__":
-	main
+	main()
